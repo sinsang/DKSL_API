57a380274cbdd1dcf436132ffe6d704393e20087
sinsang <sw980321@naver.com>
2020-09-28 19:20:47 +0900

    Update index.js
    없는 데이터 예외처리

--- index.js
+++ index.js
@@ -30,60 +30,72 @@ app.get("/getPlayerStat/:playerId", (req, res) => {
 
       conn.query("SELECT player_info.teamId, player_info.playerPhoto, player_info.playerName, player_info.wasPro, player_info.Bat_Throw, player_info.backNum, player_info.position, player_info.birthday , batter_stat.* from player_info left join batter_stat using(playerId) where player_info.playerId = " + playerId, function(e, r, f){
 
-        var player_info = {
-          teamId : r[0].teamId,
-          playerPhoto : r[0].playerPhoto,
-          playerName : r[0].playerName,
-          wasPro : r[0].wasPro,
-          position : r[0].position,
-          birthday : r[0].birthday,
-          Bat_Throw : r[0].Bat_Throw,
-          backNum : r[0].backNum
-        };
-
-        var batter_stat = {
-          total : {},
-          yearly : []
-        };
-
-        for (i in r[0]){
-          if (!(i in player_info)){
-            batter_stat.total[i] = 0;
-          }
+        if (r.length == 0){
+          res.send(undefined);
         }
+        else {
+          var player_info = {
+            teamId : r[0].teamId,
+            playerPhoto : r[0].playerPhoto,
+            playerName : r[0].playerName,
+            wasPro : r[0].wasPro,
+            position : r[0].position,
+            birthday : r[0].birthday,
+            Bat_Throw : r[0].Bat_Throw,
+            backNum : r[0].backNum
+          };
 
-        for (i in r){
+          var batter_stat = {
+            total : {},
+            yearly : []
+          };
 
-          batter_stat.yearly.push(r[i]);
-          
-          for (j in r[i]){
-            switch(j){
-              case "playerId": case "year": 
-              case "AVG": case "OBP": case "SLG": case "OPS": break;
-              default: 
-                if (!(j in player_info))
-                  batter_stat.total[j] += r[i][j];
+          for (i in r[0]){
+            if (!(i in player_info)){
+              batter_stat.total[i] = 0;
             }
           }
 
-        }
+          for (i in r){
 
-        batter_stat.total["AVG"] = batter_stat.total["H"] / batter_stat.total["AB"];
-        batter_stat.total["OBP"] = (batter_stat.total["H"] + batter_stat.total["BB"] + batter_stat.total["HBP"]) / batter_stat.total["PA"];
-        batter_stat.total["SLG"] = batter_stat.total["TB"] / batter_stat.total["AB"];
-        batter_stat.total["OPS"] = batter_stat.total["OBP"] + batter_stat.total["SLG"];
+            for (j in r[i]){
+              if (j in player_info){
+                delete r[i][j];
+              }
+            }
 
-        var result = {
-          player_info : player_info,
-          batter_stat : batter_stat
-        };
+            batter_stat.yearly.push(r[i]);
 
-        if (r[0].year == null){
-          result.batter_stat.yearly = [];
-        }
+            for (j in r[i]){
+              switch(j){
+                case "playerId": case "year": 
+                case "AVG": case "OBP": case "SLG": case "OPS": break;
+                default: 
+                  if (!(j in player_info))
+                    batter_stat.total[j] += r[i][j];
+              }
+            }
 
-        res.send(result);
-        conn.release();       
+          }
+
+          batter_stat.total["AVG"] = batter_stat.total["H"] / batter_stat.total["AB"];
+          batter_stat.total["OBP"] = (batter_stat.total["H"] + batter_stat.total["BB"] + batter_stat.total["HBP"]) / batter_stat.total["PA"];
+          batter_stat.total["SLG"] = batter_stat.total["TB"] / batter_stat.total["AB"];
+          batter_stat.total["OPS"] = batter_stat.total["OBP"] + batter_stat.total["SLG"];
+
+          var result = {
+            player_info : player_info,
+            batter_stat : batter_stat
+          };
+
+          if (r[0].year == null){
+            result.batter_stat.yearly = [];
+          }
+
+          res.send(result);   
+        }    
+
+        conn.release();
 
       });
 
@@ -110,6 +122,7 @@ app.get("/getTeamPlayers/:teamId", (req, res) => {
           conn.release();
 				}	
 				else {
+          
 					res.send(r);
           conn.release();
 				}
